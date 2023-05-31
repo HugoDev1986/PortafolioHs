6b8abf51c2f19f6b241a4d3955d96012eceae89b
HugoSanchez-2021 <victyone@gmail.com>
2023-05-31 15:13:25 -0500

    Actualizacion version responsive

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -6,16 +6,4 @@ type="text/javascript">
                 header.classList.toggle("abajo",window.scrollY>0);
             })
 
-
-/*FUNCION BOTON MENU HAMBURGUESA MOBILE*/ 
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})            
+           
